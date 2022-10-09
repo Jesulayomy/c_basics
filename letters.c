183de69578a5235d03baa3e64ad8bcea0ae35379
Jesulayomy <jesulayomi081@gmail.com>
2022-10-09 06:56:08 +0000

    numbers and letters

---
+++ letters.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// This prints out characters from the declared char tupe below and prints them.Decimal values can also be used as in 69 = E as shown
+
+char letter1= 'i';
+char letter2= 'n';
+char letter3= 't';
+char letter4= 69;
+char letter5= 'r';
+char letter6= 'n';
+
+main () {
+ printf( "%c%c%c, %c%c %c%c%c. \n", letter1, letter2, letter3, letter1, 
+	letter2, letter6, letter4, letter3); 
+ return 0;
+
+}
---
+++ numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+// Lets a go with numbers
+
+main () {
+ int a = 10;
+ int b = 17;
+
+ printf(" The numeric value of b is: %d \n", b);
+
+}
