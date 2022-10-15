001aaff9d26ce5e4b5f28682edb992df6d2479b0
Jesulayomy <jesulayomi081@gmail.com>
2022-10-15 14:53:19 +0000

    Finding roots

--- 0-loop.c
+++ 0-loop.c
@@ -1,12 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
-main () {
- int c = ' ';
- printf("Enter a character\nOr guess the letter that lets you exit\n");
- 
- while (c != 'x') {
-  c = getc(stdin);
- }
- printf("\nThat one was just obvious huh, lets see you pass the next one\n");
- return 0;
+/**
+ * main - exit strategy
+ *
+ * Return: 0 cos it worked
+ */
+int main(void)
+{
+	int c = ' ';
+
+	printf("Enter a character\nOr guess the letter that lets you exit\n");
+
+	while (c != 'x')
+	{
+		c = getc(stdin);
+	}
+
+	printf("\nThat one was just obvious huh, lets see you pass the next one\n");
+	return (0);
 }
