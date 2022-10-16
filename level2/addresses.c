608bfd18884920b3d004e6195b1d7512953ff304
Jesulayomy <jesulayomi081@gmail.com>
2022-10-16 17:29:28 +0000

    Addresses

--- level2/addresses.c
+++ level2/addresses.c
@@ -10,7 +10,8 @@ int main(void)
 	int x = 12;	/** Initializzed a variable */
 	int *ax = &x;	/* made a pointer, whose value is the address of x */
 
-	printf("The value of x is: %d, and its address is %p.\n", x, ax);
+	printf("The value of x is: %d, and its address is %p.\n", x, &x);
+	printf("ax is a pointer with value: %p, and its address is %p.\n", ax, &ax);
 
 	return (0);
 }
