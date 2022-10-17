4cee1b9b4b0d2721c523a124fdfe943ff038f4f6
Jesulayomy <jesulayomi081@gmail.com>
2022-10-17 17:41:21 +0000

    Updated for imaginary roots.

--- level3/quadratic.c
+++ level3/quadratic.c
@@ -6,7 +6,7 @@
  * @b: - for b
  * @c: - for c
  *
- * This code collects values of a quadratic equation and evaluates it
+ * This code collects values of a quadratic equation and evaluates it works for all equations now
  *
  * Return: done1 and done2 as values for x1 and x2
  */
@@ -14,15 +14,31 @@ float quad(float a, float b, float c)
 {
 	float disc2, disc, root, left, right, done1, done2;
 	disc = ((b * b) - (4 * a * c));
-	float n = disc;
+
 	if (disc < 0)
 	{
-		printf("This equation has no real roots. Dont break me.\n");
+		printf("This equation has no real roots.\n");
+		printf("Equation: (%.2f)x*x + (%.2f)x + (%.2f) = 0\n", a, b, c);
+		printf("The Discriminant is: %.2f\n", disc);
+		disc = -disc;
+		float n = disc;
+		while ((disc - (n / disc)) > 0.00001)
+		{
+			disc = ((disc + (n / disc)) / 2);
+		}
+		root = disc;
+		printf("root of the disc is: %fi\n", root);
+		left = ((-b) / (2 * a));
+		right = (root / (2 * a));
+
+		printf("The solutions to the equation are x = %.2f + (%.2f)i and %.2f - (%.2f)i\n", left, right, left, right);
 	}
 	else
 	{
 		printf("Equation: (%.2f)x*x + (%.2f)x + (%.2f) = 0\n", a, b, c);
 		printf("The Discriminant is: %.2f\n", disc);
+		float n = disc;
+
 		while ((disc - (n / disc)) > 0.00001)
 		{
 			disc = ((disc + (n / disc)) / 2);
